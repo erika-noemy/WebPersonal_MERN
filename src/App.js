a9d83bbf92dc0b12f8f11b02514ac722edc1dbd0
Erika <erikanoemychuya@gmail.com>
2022-05-04 10:12:31 -0400

    error404 y links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 
 import './App.scss';
 import {BrowserRouter as Router, Route , Routes, Link} from 'react-router-dom';
-
 function App() {
-  
   return (
     <Router>
       <div className="App">
        <h1>Sistema de rutas basicas</h1>
+       <Link to="/">  HOME</Link> <br/>
+       <Link to="/user">USERS</Link><br/>
+       <Link to="/contact">CONTACT</Link>
        <Routes>
          <Route exact path = "/" element = {<Home/>} />
          <Route exact path = "/contact" element = {<Contact/>} />
          <Route exact path = "/user" element = {<User/>} />
+         <Route path='*' element = {<Error404/>} />
        </Routes>
       
       </div>
@@ -19,7 +21,6 @@ function App() {
     
   );
 }
-
 function Home(){
   return <h2> estamos en el componente home</h2>;
 }
@@ -28,5 +29,8 @@ function Contact(){
 }
 function User(){
   return <h2> estamos en el componente user</h2>;
+}
+function Error404 (){
+  return <h2> Error404</h2>;
 }
 export default App;
