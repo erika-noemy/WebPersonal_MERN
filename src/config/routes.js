45d06116315bedbd903c86fc0af4c00763aaed60
Erika <erikanoemychuya@gmail.com>
2022-05-04 11:02:50 -0400

    creando rutas para el panel administrador

---
+++ src/config/routes.js
@@ -0,0 +1,25 @@
+import LayoutAdmin from "../layouts/LayoutAdmin";
+// admin pages
+import AdminHome from '../pages/Admin';
+import AdminSignIn from '../pages/Admin/SignIn';
+
+const routes = [
+    {
+        path: "/admin",
+        component : LayoutAdmin,
+        exact: false,
+        routes: [
+            {
+                path : '/admin',
+                component: AdminHome,
+                exact: true
+            },
+            {
+                path: "/admin/login",
+                component: AdminSignIn,
+                exact: true
+            }
+        ]
+    }
+]
+export default routes;
--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -5,9 +5,8 @@ export default function LayoutAdmin(){
         <div>
             <Layout>
                 <h2>Menu Sider</h2>
-                <div>
-                    contenido ...
-                </div>
+                <div> contenido ... </div>
+                <h5>Footer </h5>
             </Layout>
         </div>
     )
