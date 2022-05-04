45ed7ac14f9092ebd7b7c6fc0f7467985eaa9007
Erika <erikanoemychuya@gmail.com>
2022-05-04 10:41:59 -0400

    creando paginas basicas para usar en las rutas

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 
 import './App.scss';
-import {BrowserRouter as Router, Route , Routes, Link} from 'react-router-dom';
+import Admin from './pages/Admin';
+import SignIn from './pages/Admin/SignIn';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 function App() {
   return (
-    <Router>
-      <div className="App">
-       <h1>Sistema de rutas basicas</h1>
-       <Link to="/">  HOME</Link> <br/>
-       <Link to="/user">USERS</Link><br/>
-       <Link to="/contact">CONTACT</Link>
-       <Routes>
-         <Route exact path = "/" element = {<Home/>} />
-         <Route exact path = "/contact" element = {<Contact/>} />
-         <Route exact path = "/user" element = {<User/>} />
-         <Route path='*' element = {<Error404/>} />
-       </Routes>
-      
-      </div>
-    </Router>
+    <div>
+      <h1>estamos en App.js</h1>
     
+      <Admin/>
+      <SignIn/>
+      <Home/>
+      <Contact/>
+    </div>
+     
+  
+   
   );
-}
-function Home(){
-  return <h2> estamos en el componente home</h2>;
-}
-function Contact(){
-  return <h2> estamos en el componente contact</h2>;
-}
-function User(){
-  return <h2> estamos en el componente user</h2>;
-}
-function Error404 (){
-  return <h2> Error404</h2>;
 }
 export default App;
---
+++ src/pages/Admin/Admin.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Admin(){
+    return <h1>Estamos en admin</h1>
+}
